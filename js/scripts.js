cdc7b9419609f8d403d35392e597dd8494293c8e
Ainur Dzhaianbaeva and Tim Firman <tim@>
2018-01-16 13:23:04 -0800

    adds multiple flavor output

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,15 @@ $(document).ready(function() {
 
     userInputs.forEach(function(formInputId) {
       var flavorInputString = $("input#" + formInputId).val();
-      //userInput = "chocolate";
+
       flavorlist.push(flavorInputString);
     });
 
-    console.log(flavorlist);
 
     flavorlist.forEach(function(flavor) {
-      //what to do with the flavor??
-      console.log("The next flavor to print is: " + flavor);
 
-    $("#output").text(flavor);
+
+    $("#output").append(flavor + " ");
 
     });
 
